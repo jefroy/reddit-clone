c4a15bec8be563b47be80d7da78548bc05fd9c62
ajay sieunarine <ajay_sieunarine@hotmail.com>
2023-04-04 00:49:14 -0400

    Modal - add auth modal skeleton

---
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -0,0 +1,42 @@
+// https://chakra-ui.com/docs/components/modal
+
+import React from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton, Button, useDisclosure,
+} from '@chakra-ui/react'
+
+
+const AuthModal : React.FC = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+        <>
+            <Button onClick={onOpen}>Open Modal</Button>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <p>wow</p>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant='ghost'>Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
+}
+
+export default AuthModal;
